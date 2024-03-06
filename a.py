3514783e4ff91ec8362bb4a271913ed2bfa5e83d
sim <simyechan87@gmail.com>
2024-03-06 10:03:39 +0900

    백준 25083번

---
+++ a.py
@@ -0,0 +1,30 @@
+# 다각형
+
+# N, M = input().split()
+# n = int(N)
+# m = int(M)
+# a = [0] * (n+m)
+# max = 0
+
+# for i in range (1, n+1):
+#     for j in range (1, m+1):
+#         a[i+j] = a[i+j]+1
+
+# for i in range(n+m+1):
+#     if a[i] > max:
+#         max = a[i]
+
+# for i in range(n+m+1):
+
+
+
+
+
+
+# n  = int(input())
+# a = []
+# for i in range(1, n+1):
+#     a.append(i)
+
+# for j in range(1, n+1, i):
+#     print(i)
