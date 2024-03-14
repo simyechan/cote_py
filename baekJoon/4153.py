25816fa72cb3b501422318b08e14e34a1a0ac569
sim <simyechan87@gmail.com>
2024-03-14 17:05:39 +0900

    백준4153번

---
+++ baekJoon/4153.py
@@ -0,0 +1,10 @@
+# 4153
+
+while(1):
+  a, b, c = map(int, input().split())
+  if a == 0 and b == 0 and c == 0:
+    break
+  if a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or c**2 + a**2 == b**2:
+    print('right')
+  else:
+    print('wrong')
