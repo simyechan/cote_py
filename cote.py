d72b4b0c91f368b4a9b9f615f78b8721ce1221cc
sim <simyechan87@gmail.com>
2024-05-08 19:44:54 +0900

    추가

--- cote.py
+++ cote.py
@@ -37,14 +37,62 @@
 
 
 # 카드
-n = int(input())
-ls = []
+# n = int(input())
+# ls = []
 
-for i in range(1, n+1):
-  ls.append(i)
+# for i in range(1, n+1):
+#   ls.append(i)
 
-while len(ls) > 1:
-  ls.pop(0)
-  ls.append(ls.pop(0))
+# while len(ls) > 1:
+#   ls.pop(0)
+#   ls.append(ls.pop(0))
 
-print(ls)
+# print(ls)
+
+
+
+# 후위 표기식2
+# c = []
+# num = []
+# n = []
+# n1 = n2 = 0
+# n = int(input())
+
+# ar = input()
+# for i in ar:
+#   c.append(i)
+
+# for _ in range(n):
+#   num.append(int(input()))
+
+# for i in c:
+#   if i == '*':
+#     n2 = num.pop()
+#     n1 = num.pop()
+#     num.append(n1 * n2)
+#   elif i == '/':
+#     n2 = num.pop()
+#     n1 = num.pop()
+#     num.append(n1 / n2)
+#   elif i == '+':
+#     n2 = num.pop()
+#     n1 = num.pop()
+#     num.append(n1 + n2)
+#   elif i == '-':
+#     n2 = num.pop()
+#     n1 = num.pop()
+#     num.append(n1 - n2)
+#   else:
+#     print(num[c.index(i)])
+
+# print(num)
+
+# 5
+# ABC*+DE/-
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# 6.20
